3dac6f5a30a5cdae86f555e354be6d0feda3e3be
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2020-10-25 21:31:59 -0300

    final
    - comentários
    - cálculo da data

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 
+//variáveis para usar no post do comentário do usuário
 username = "";
 userAvatar = "";
 postUuid = "";
 
+//função de inicialização, recebe informações do post e do usuário atual
 (function(apiUrl) {
+
+  //função que recebe o usuário atual
     function getMe() {
       return fetch(apiUrl + "/me")
         .then(function(response) {
           return response.json();
         })
         .then(function(user) {
-          const $username = document.getElementById("current-user-username");
-          const $avatar = document.getElementById("current-user-avatar");
+          const $username = document.getElementById("current-user-username"); //div que receberá o username para mostrar na página
+          const $avatar = document.getElementById("current-user-avatar");     //div que receberá o avatar para mostrar na página
   
           username = user.username;
           $username.innerHTML = user.username;
   
           if (user.avatar) {
-            
             avatar = user.avatar;
             $avatar.style.backgroundImage = "url('" + user.avatar + "')";
           }
         });
     }
 
+    //função que recebe as informações do post
     function getPost() {
         return fetch(apiUrl + "/post")
           .then(function(response) {
             return response.json();
           })
           .then(function(post) {
+
+            //divs da página que receberão as informações
             const $username = document.getElementById("publisher-info-name");
             const $avatar = document.getElementById("publisher-avatar");
             const $location = document.getElementById("publisher-info-location");
@@ -44,16 +50,21 @@ postUuid = "";
             }
 
             $location.innerHTML = post.location.city + ", " + post.location.country;
+
             $image.style.backgroundImage = "url('" + post.photo + "')";
+
+            //tratando a data para mostrar "dia de mês" ("9 de outubro", por exemplo)
             date = new Date(post.created_at);
             months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
             $date.innerHTML = date.getDate()  + " de " + months[date.getMonth()];
+
             $comments_qt.innerHTML = post.comments.length + " comentários";
 
             post.comments.forEach(comment => {
               buildComment(comment.user.avatar, comment.user.username, comment.message, new Date(comment.created_at));
             }); 
 
+            //guarda o uuid do post para quando for salvar o comentário escrito pelo user
             postUuid = post.uuid;
 
           });
@@ -67,42 +78,54 @@ postUuid = "";
     initialize();
   })("https://taggram.herokuapp.com");
 
-  async function  sendComment(apiUrl) {
+  //função que manda o comentário para o banco de dados
+  function  sendComment(apiUrl) {
 
+    //pega o valor do comentário digitado
     var message = document.getElementById("textfield").value;
+
+    //informações para mandar 
     let body = { username: username , message: message };
-    var myInit = { method: 'POST', headers: {'Content-Type': 'application/json;charset=utf-8'}, body: JSON.stringify(body) };
+
+    var init = { method: 'POST', headers: {'Content-Type': 'application/json;charset=utf-8'}, body: JSON.stringify(body) };
     var url = apiUrl + "/posts/" + postUuid + "/comments"
 
     try {
-      const response = await fetch(url, myInit);
+      const response = await fetch(url, init);
+
+      //verifica se não deu erro
       if (!response.ok) {
         return new Error('falhou a requisição');
       }
+
       const comments = await response.json();
       const $comments = document.getElementById("comments");
       $comments.innerHTML = "";
 
-      console.log(comments);
-
+      //recarrega os comentários
       comments.forEach(comment => {
         buildComment(comment.user.avatar, comment.user.username, comment.message, new Date(comment.created_at));
       });
+
     } catch (err) {
       window.alert("Houve um erro ao enviar seu comentário. Por favor tente novamente!");
-      console.log(err.message);
     }
   }
 
+  //função construtora do comentário
   function buildComment(avatar, name, message, created_at){
     
+    //referência da área de comentários
     const $comments = document.getElementById("comments");
+
+    //criação de divs para estruturação
     var comment_container = document.createElement("DIV");
     var comment = document.createElement("DIV");
     var avatar1 = document.createElement("DIV");
     var comment_info = document.createElement("DIV");
     var date = document.createElement("DIV");
 
+    //atribuição das classes (css)
     comment_info.setAttribute("class", "message");
     comment_container.setAttribute("class", "comment-container");
     comment.setAttribute("class", "comment");
@@ -118,22 +141,25 @@ postUuid = "";
     created_at = new Date(created_at);
     date.innerHTML = created_at.getDate();
 
+    //cálculo do tempo decorrido da postagem até a data presente
     const now = Date.now();
-    var timeDiff = Math.abs(now - created_at);
-    var diff = Math.ceil(timeDiff / (1000 * 60)); 
-    var unit = "m";
+    var timeDiff = Math.abs(now - created_at.getTime());
+    var diff = Math.ceil(timeDiff / (1000 * 60)); //minutos
+    var unit = "min";
 
     if(diff >= 60){
-      diff = Math.ceil(diff/60);
+      diff = Math.ceil(diff/60); //horas
       unit = "h";
-    }
-    if(diff >= 24){
-      diff = Math.ceil(diff/24);
-      unit = "d";
-    }
-    if(diff >= 7){
-      diff = Math.ceil(diff/7);
-      unit = "w";
+
+      if(diff >= 24){
+        diff = Math.ceil(diff/24); //dias
+        unit = "d";
+
+        if(diff >= 7){
+          diff = Math.ceil(diff/7); //semanas
+          unit = "w";
+        }
+      }
     }
 
     date.innerHTML = diff+unit;
@@ -142,6 +168,5 @@ postUuid = "";
     comment.appendChild(comment_info);
     comment_container.appendChild(comment);
     comment_container.appendChild(date);
-
     $comments.appendChild(comment_container);
   }
